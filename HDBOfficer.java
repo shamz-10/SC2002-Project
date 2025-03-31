ac475291ffa24c46d572c9e82e57f4a3c533467f
shammas <167613297+shamz-10@users.noreply.github.com>
2025-03-31 19:27:57 +0800

    Update HDBOfficer.java

--- HDBOfficer.java
+++ HDBOfficer.java
@@ -1,4 +1,8 @@
 // HDBOfficer class extending Applicant
+import java.util.List;
+import java.util.ArrayList;
+
+
 public class HDBOfficer extends Applicant {
     private Project assignedProject;
     private String registrationStatus;
